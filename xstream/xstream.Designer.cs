9f94cc0d6807a90441de9c1aab97d132d7df20e5
MutoMagic <8569775+MutoMagic@users.noreply.github.com>
2020-08-06 02:38:58 +0800

    add lib

--- xstream/xstream.Designer.cs
+++ xstream/xstream.Designer.cs
@@ -28,10 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // xstream
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Name = "xstream";
+            this.Text = "xStream";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
