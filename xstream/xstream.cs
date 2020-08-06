b73105f8886bf3e0ef998cba1e91a14eaa3b9fc6
MutoMagic <8569775+MutoMagic@users.noreply.github.com>
2020-08-07 06:35:40 +0800

    remote test

--- xstream/xstream.cs
+++ xstream/xstream.cs
@@ -1,5 +1,8 @@
 ﻿using SmartGlass;
 using SmartGlass.Common;
+using SmartGlass.Nano;
+using SmartGlass.Nano.Consumer;
+using SmartGlass.Nano.Packets;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -40,6 +43,40 @@ namespace xstream
 
             GamestreamSession session = await client.BroadcastChannel.StartGamestreamAsync(config);
             Console.WriteLine($"Connecting to NANO // TCP: {session.TcpPort}, UDP: {session.UdpPort}");
+
+            NanoClient nano = new NanoClient(addressOrHostname, session);
+
+            // General Handshaking & Opening channels
+            Console.WriteLine($"Running protocol init...");
+            await nano.InitializeProtocolAsync();
+
+            // Start Controller input channel
+            await nano.OpenInputChannelAsync(1280, 720);
+
+            // Start ChatAudio channel
+            //AudioFormat chatAudioFormat = new AudioFormat(1, 24000, AudioCodec.Opus);
+            //await nano.OpenChatAudioChannelAsync(chatAudioFormat);
+
+            Console.WriteLine("Adding FileConsumer");
+            //IConsumer consumer = /* initialize consumer */;
+            //nano.AddConsumer(consumer);
+
+            // Start consumer, if necessary
+            //consumer.Start();
+
+            // Audio & Video client handshaking
+            // Sets desired AV formats
+            Console.WriteLine("Initializing AV stream (handshaking)...");
+            AudioFormat audioFormat = nano.AudioFormats[0];
+            VideoFormat videoFormat = nano.VideoFormats[0];
+            await nano.InitializeStreamAsync(audioFormat, videoFormat);
+
+            // Tell console to start sending AV frames
+            Console.WriteLine("Starting stream...");
+            await nano.StartStreamAsync();
+
+            /* Run a mainloop, to gather controller input events or similar */
+            Console.WriteLine("Stream is running");
 
             return 0;
         }
